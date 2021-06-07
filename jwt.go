26695c659c69febc908a5bdaade4e8f112882e65
HadyElzayady <hadyelzayady@yahoo.com>
2021-06-07 14:56:32 +0200

    some fns

--- jwt.go
+++ jwt.go
@@ -1,11 +1,37 @@
 package jwt
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+)
+
 type Payload struct {
 	username string
 }
 
+type header struct {
+}
+
+func newHeaderEncoded() string {
+
+	return "header"
+}
+
+func newPayloadEncoded() string {
+	return "payload"
+}
+
+func newSignature(h string, p string, s string) string {
+	return "sign"
+}
+
 func CreateToken(payload *Payload) (string, error) {
-	return "123", nil
+	encoded_header := newHeaderEncoded()
+	encoded_payload := newPayloadEncoded()
+	secret := "secret"
+	signature := newSignature(encoded_header, encoded_payload, secret)
+	token := "." + signature
+	return token, nil
 }
 
 func VerifyToken(token string) (*Payload, error) {
